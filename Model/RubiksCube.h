3a5148f9de6b6c1e0bdae3614944f93d500628a4
Sasank <sasanknasika555@gmail.com>
2025-05-16 18:08:32 +0530

    ...

--- Model/RubiksCube.h
+++ Model/RubiksCube.h
@@ -71,9 +71,5 @@ enum class MOVE {
         virtual RubiksCube &bPrime() = 0;
 
         virtual RubiksCube &b2() = 0;
-        string getCornerColorString(uint8_t ind) const;
 
-        uint8_t getCornerIndex(uint8_t ind) const;
-
-        uint8_t getCornerOrientation(uint8_t ind) const;
 };
